354c00a7343d39c640206b166a564635bd3576cd
jostsalathe <jostsalathe@gmail.com>
2024-10-06 15:43:10 +0200

    forgot new files...

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Blink(1) Apps
+# Blink(1) Apps
 
 This is a collection of the tools I created for my blink(1) devices.
 
---
+++ install_venv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 -m venv ./myvenv
+./myvenv/bin/pip install blink1
+./myvenv/bin/pip install psutil
+
